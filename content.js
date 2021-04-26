88e0a958e91cb60a99b58fe6fa08697a1ecf8f47
Jose Atlin <atlinbabu@gmail.com>
2021-04-26 21:35:39 +0530

    alert styling

--- content.js
+++ content.js
@@ -34,30 +34,43 @@ function fade(el, speed) {
 }
 
 function sendAlert(text) {
+	
 	var el;
-
 	el = document.createElement('div');
-	el.style.fontFamily = 'Helvetica, sans-serif';
+
+	// font
+	el.style.fontFamily = 'sans-serif';
+	el.style.fontSize = '100%';
 	el.style.fontStyle = 'normal';
 	el.style.fontWeight = 'normal';
-	el.style.boxShadow = '0px 0px 16px 0px #CBCBCB';
+
+	// text
+	el.innerHTML = text;
+	el.style.boxSizing = 'content-box';
 	el.style.border = '1px solid #D9D900';
-	el.style.zIndex = '100000001';
+	el.style.borderRadius = '.25em';
 	el.style.textAlign = 'center';
 	el.style.color = '#444444';
 	el.style.backgroundColor = '#FFFF5C';
 	el.style.position = 'fixed';
-	el.style.borderRadius = '.25em';
-	el.innerHTML = text;
-	el.style.boxSizing = 'content-box';
-	el.style.height = '2em';
-	el.style.lineHeight = '2em';
-	el.style.width = '7em';
-	el.style.padding = '0px';
-	el.style.margin = '0px';
+	
+	el.style.display = 'flex';
+	el.style.justifyContent = 'center';
+	el.style.alignItems = 'center';
 
-	el.style.top = '25px';
-	el.style.left = '25px';
+    el.style.top = "55%";
+    el.style.left = "55%";
+    el.style.width = "30em";
+    el.style.height = "10em";
+	el.style.marginTop = '-9em';
+    el.style.marginLeft = '-15em';
+
+	// el.style.padding = '50px';
+	// el.style.height = '2em';
+	// el.style.lineHeight = '2em';
+	// el.style.width = '7em';
+	// el.style.margin = '50px';
+
 
 	document.body.appendChild(el);
 	duration = 750;
